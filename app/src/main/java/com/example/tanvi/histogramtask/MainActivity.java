7fea62f37b7c259d25c72c0b781addd1f89c30bb
Tanvi Goyal <goyaltanvi94@gmail.com>
2018-12-18 12:20:54 +0530

    Added Images Option- Gallery, Camera

--- app/src/main/java/com/example/tanvi/histogramtask/MainActivity.java
+++ app/src/main/java/com/example/tanvi/histogramtask/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.tanvi.histogramtask;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import org.opencv.android.OpenCVLoader;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends BaseActivity {
 
@@ -32,5 +48,93 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        requestPermission();
     }
+
+    private void requestPermission() {
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE) ||
+                    ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+
+                Snackbar.make(root, getResources().getString(R.string.request_permission_rationale), Snackbar.LENGTH_INDEFINITE)
+                        .setAction(getResources().getString(R.string.ok), new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                ActivityCompat.requestPermissions(MainActivity.this, requestedPermissions, PERMISSIONS_REQUEST);
+                            }
+                        }).show();
+            } else {
+                ActivityCompat.requestPermissions(this, requestedPermissions, PERMISSIONS_REQUEST);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull final String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST: {
+                if (grantResults.length > 0 && grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Snackbar.make(root, getResources().getString(R.string.request_permission_rationale), Snackbar.LENGTH_INDEFINITE)
+                            .setAction(getResources().getString(R.string.ok), new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    ActivityCompat.requestPermissions(MainActivity.this, requestedPermissions, PERMISSIONS_REQUEST);
+                                }
+                            }).show();
+                }
+            }
+        }
+    }
+
+    @OnClick(R.id.add_image)
+    void onClick() {
+
+        final CharSequence[] items = {"Camera" , "Gallery" , "Cancel"};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Add an Image");
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if(items[which].equals("Camera")){
+
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent, REQUEST_CAMERA);
+
+                }else if(items[which].equals("Gallery")){
+
+                    Intent intent = new Intent(Intent.ACTION_PICK , MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    intent.setType("image/*");
+                    startActivityForResult(intent.createChooser(intent,"Select File"), SELECT_FILE);
+
+                }else if(items[which].equals("Cancel")){
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode , int resultCode , Intent data){
+        super.onActivityResult(requestCode , resultCode , data);
+
+        if(resultCode == Activity.RESULT_OK){
+            if(requestCode == REQUEST_CAMERA ) {
+
+                Bundle bundle = data.getExtras();
+                final Bitmap bmp = (Bitmap) bundle.get("data");
+                sampleImage.setImageBitmap(bmp);
+
+            }else if(requestCode == SELECT_FILE){
+
+                Uri selectImg = data.getData();
+                sampleImage.setImageURI(selectImg);
+            }
+        }
+    }
+
 }
